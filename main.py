4a712aeedb0855feb890d948f45e643e3e3fe594
Eugene Mironov <mir-evgenii@mail.ru>
2020-02-08 15:29:13 +0300

    Added point3d class with distanse function.

---
+++ main.py
@@ -0,0 +1,9 @@
+from point3d import point3d
+
+p3d1 = point3d(1.1, 2.2, 3.3)
+p3d2 = point3d(1.2, 2.3, 3.4)
+
+res = p3d1.distanse_to(p3d2)
+
+print(res)
+
---
+++ point3d.py
@@ -0,0 +1,45 @@
+import math
+
+class point3d:
+
+    def __init__(self, x = 0.0, y = 0.0, z = 0.0):
+
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_z(self):
+        return self.z
+
+    def set_x(self, x = 0.0):
+        self.x = x
+
+    def set_y(self, y = 0.0):
+        self.y = y
+
+    def set_z(self, z = 0.0):
+        self.z = z
+
+    def distanse_to(self, Point3d):
+
+        x = (self.x - Point3d.get_x()) ** 2
+        y = (self.y - Point3d.get_y()) ** 2
+        z = (self.z - Point3d.get_z()) ** 2
+
+        dist = math.sqrt(x + y + z)
+
+        return dist
+
+    def geron(self, x, y, z):
+        p = (a+b+c)/2
+        print ('Полупериметр =', p )
+        s = (p*(p-a)*(p-b)*(p-c))**0.5
+        return s
+
+
